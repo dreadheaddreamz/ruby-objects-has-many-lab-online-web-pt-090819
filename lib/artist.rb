1a0de93934ece5b4ae623c6e61c2a3a6d9620cbc
James Batsell <jamesbatsell05@gmail.com>
2019-10-09 01:27:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,4 +7,21 @@ class Artist
     @songs = []
   end
   
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song_by_name(song)
+    song = Song.new(song)
+    add_song(song)
+  end
+  
+  def self.song_count
+    Song.all.length
+  end
+end
   
