969ebd9e6e60eb05f03b244f146bece37088bac1
James Batsell <jamesbatsell05@gmail.com>
2019-10-09 01:57:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,16 +7,16 @@ class Author
     @title = []
   end
   
-  def add_title(title)
-    title.author = self
+  def add_posts(posts)
+    posts.author = self
   end
   
-  def title
-    title.all.select {|title| title.author == self}
+  def posts
+    posts.all.select {|posts| posts.author == self}
   end
   
   def add_post_by_title(title)
-    song = Title.new(title)
+    posts = Post.new(title)
     add_post(title)
   end
   
